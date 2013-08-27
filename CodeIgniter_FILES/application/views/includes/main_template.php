e110b8298bb29fcb0d0852610c717f9a05977dd0
Fatine <fatine.nakkoubi@outlook.fr>
2013-08-27 16:39:19 +0200

    suppression de fichier inutiles

--- CodeIgniter_FILES/application/views/includes/main_template.php
+++
@@ -1,7 +0,0 @@
-<?php $this->load->view( 'includes/header' ); ?>
-
-<div id="main-content">
-    <?php $this->load->view( $main_content ); ?>
-</div>
-
-<?php $this->load->view( 'includes/footer' ); ?>
